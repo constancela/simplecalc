9d961acb73b16922de94a985ab36885e7f9ac270
Constance La <constancexla@gmail.com>
2018-01-15 02:49:09 -0800

    finished code

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,40 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        let count = args.count
+        var result = 0
+        
+        if count == 1 {
+            result = 0
+        } else if args[count - 1] == "count" {
+            result = count - 1
+        } else if args[count - 1] == "avg" {
+            var total = 0
+            var copy = args
+            copy.remove(at: count - 1)
+            for nums in copy {
+                total += Int(nums)!
+            }
+            result = total / copy.count
+        } else if args[count - 1] == "fact" {
+            var total = 1
+            for index in 1...Int(args[0])! {
+                total = total * index
+            }
+            result = total
+        } else if args[1] == "+" {
+            result = Int(args[0])! + Int(args[2])!
+        } else if args[1] == "-" {
+            result = Int(args[0])! - Int(args[2])!
+        } else if args[1] == "*" {
+            result = Int(args[0])! * Int(args[2])!
+        } else if args[1] == "/" {
+            result = Int(args[0])! / Int(args[2])!
+        } else if args[1] == "%" {
+            result = Int(args[0])! % Int(args[2])!
+        }
+
+        return result;
     }
     
     public func calculate(_ arg: String) -> Int {
